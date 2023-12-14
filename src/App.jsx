270972f37dca6aa5923d7991fa29f6b2ade4a18e
mdnazmusakib <md.nazmusakib02@gmail.com>
2023-12-14 12:56:25 +0600

    navbar done home page half

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
+import { Navbar } from './Components/Navbar'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import { Home } from './Pages/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Navbar.css'
-
-export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
